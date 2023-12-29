c6452fc1750bdc682d7984e00b2b2ad80127cf32
Oscar Ryley <oscar.ryley@gmail.com>
2023-12-29 21:18:28 +0000

    Started developing shooting charms
    Wizard can now shoot charm objects to the right with the right arrow key.
    
    Need to stop this happening more than once every half a second.
    Need to allow for shooting up, down and left - possibly with different calls of the charm instance.

--- Godot/Wizard.gd
+++ Godot/Wizard.gd
@@ -1,9 +1,11 @@
 extends CharacterBody2D
 
+const bulletpath = preload("res://Godot/Charm.tscn")
+
 var speed = 150
 
 func _process(delta):
-	Global.wizard_position = position
+	Global.wizard_position = position - Vector2(681,362)
 	
 	if Input.is_key_pressed(KEY_D):
 		position.x += delta * speed
@@ -13,12 +15,8 @@ func _process(delta):
 		position.y -= delta * speed
 	if Input.is_key_pressed(KEY_S):
 		position.y += delta * speed
-		
+
 	if Input.is_action_pressed("ui_right"):
-		pass
-	if Input.is_action_pressed("ui_left"):
-		pass
-	if Input.is_action_pressed("ui_up"):
-		pass
-	if Input.is_action_pressed("ui_down"):
-		pass
+		var bullet = bulletpath.instantiate()
+		get_node("/root/Scene").add_child(bullet)
+		bullet.position = position
--- Godot/global.gd
+++ Godot/global.gd
@@ -3,6 +3,7 @@ extends Node
 @export var spawn_zombie_object = preload("res://Godot/Zombie.tscn")
 var spawn_pos_list = [Vector2(-675,-350), Vector2(675, 350), Vector2(-675, 350), Vector2(675, -350)]
 var wizard_position = Vector2(0,0)
+var zombies = 0
 
 func spawn_zombie():
 	var obj = spawn_zombie_object.instantiate()
@@ -15,8 +16,13 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	#spawn_zombie()
-	pass
+	if zombies == 20:
+		get_node("Spawn_timer").wait_time = 4
+	elif zombies == 60:
+		get_node("Spawn_timer").wait_time = 3
+	elif zombies == 150:
+		get_node("Spawn_timer").wait_time = 2
 
 func _on_timer_timeout():
 	spawn_zombie()
+	zombies += 1
