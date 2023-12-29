36b1583c96c71fc03621012583217a9d7429c84a
Oscar Ryley <oscar.ryley@gmail.com>
2023-12-29 17:03:13 +0000

    Working on Spawning
    Zombies still need to move constantly towards player character

--- Godot/Wizard.gd
+++ Godot/Wizard.gd
@@ -3,11 +3,20 @@ extends CharacterBody2D
 var speed = 150
 
 func _process(delta):
-	if (Input.is_action_pressed("ui_right") or Input.is_key_pressed(KEY_D)):
+	if Input.is_key_pressed(KEY_D):
 		position.x += delta * speed
-	if (Input.is_action_pressed("ui_left") or Input.is_key_pressed(KEY_A)):
+	if Input.is_key_pressed(KEY_A):
 		position.x -= delta * speed
-	if (Input.is_action_pressed("ui_up") or Input.is_key_pressed(KEY_W)):
+	if Input.is_key_pressed(KEY_W):
 		position.y -= delta * speed
-	if (Input.is_action_pressed("ui_down") or Input.is_key_pressed(KEY_S)):
+	if Input.is_key_pressed(KEY_S):
 		position.y += delta * speed
+		
+	if Input.is_action_pressed("ui_right"):
+		pass
+	if Input.is_action_pressed("ui_left"):
+		pass
+	if Input.is_action_pressed("ui_up"):
+		pass
+	if Input.is_action_pressed("ui_down"):
+		pass
--- Godot/global.gd
+++ Godot/global.gd
@@ -1,11 +1,17 @@
 extends Node
 
+@export var spawn_zombie = preload("res://Godot/Zombie.tscn")
+var spawn_pos_list = [Vector2(-675,-350), Vector2(675, 350), Vector2(-675, 350), Vector2(675, -350)]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	while true:
+		await get_tree().create_timer(5).timeout
+		var obj = spawn_zombie.instantiate()
+		obj.position = spawn_pos_list[randi() % spawn_pos_list.size()]
+		add_child(obj)
